1d1be788b58c257597c480aa586454e299084f2e
liliansantos15 <goncalves.santos.lilian@escola.pr.gov.br>
2025-11-12 08:06:16 -0300

    .......

--- app.js
+++ app.js
@@ -14,4 +14,12 @@ cartao.innerHTML = `
 <p>${resposta}</p>
 </div>
 </div>
+let respostaEstaVisivel=false
 
+  function viraCartao(){
+     respostaEstaVisivel=!respostaEstaVisivel
+     cartao.classList.toggle('active', respostaEstaVisivel)
+  }
+     cartao.addEventListener('click', viraCartao)
+     container.appendChild(cartao)
+}
